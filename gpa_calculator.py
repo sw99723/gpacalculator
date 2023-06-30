bfc04135c4184ba7149ba7437558899bae92e4be
sw99723 <48198355+sw99723@users.noreply.github.com>
2023-06-29 23:59:19 -0400

    Update gpa_calculator.py

--- gpa_calculator.py
+++ gpa_calculator.py
@@ -16,6 +16,7 @@ grade_scheme = {"A+": {"Grade Point": 4.0, "Percentage": (90, 100)},
                 "F": {"Grade Point": 0.0, "Percentage": (0, 49)}}
 
 # taken_courses 예시
+# ANT102H5: 66, MAT102H5: 56, ANT101H5: 69, CSC108H5: 77, SOC100H5: 53, MAT135H5: 73, MAT136H5: 76, MAT223H5: 68, MAT232H5: 75, STA256H5: 68, RLG203H5: 78
 '''              {'ANT102H5': 66, 'MAT102H5': 56, 'ANT101H5': 69, 'CSC108H5': 77,
                  'SOC100H5': 53, 'MAT135H5': 73, 'MAT136H5': 76, 'MAT223H5': 68,
                  'MAT232H5': 75, 'STA256H5': 68, 'RLG203H5': 78}'''
@@ -26,9 +27,6 @@ def input_grades():
     st.write("Enter your course information:")
     st.write("Format: Course Name: Score")
     st.write("Example: MAT135H5: 73")
-    st.write("Leave a blank line to finish entering grades.")
-
-    course_info = st.text_area("Course Information")
 
     taken_courses = {}
 
